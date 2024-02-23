7bc04f284695d84464d011972dff0c07c31c18b9
Oscar Perez <100051211+Astro024@users.noreply.github.com>
2024-02-23 06:14:52 +0000

    done

--- main.py
+++ main.py
@@ -5,7 +5,16 @@ def main():
     Use m_perc and f_perc for your results
     ##################################################
     """
+    maleS = int(input("Males in class: "))
+    femaleS = int(input("Females in class: "))
+    sum = int (maleS + femaleS)
+    total = int (sum)
+    m_perc = float ((maleS / sum) * 100)
+    f_perc = float ((femaleS / sum) * 100)
 
+    print ("The total amount of students is: ", total)
+    print ('The percentage of males is',int(round(m_perc)),'%')
+    print ('The percentage of females is',int(round(f_perc)),'%')
     """
     ########################################
     # Do not delete the return statement
